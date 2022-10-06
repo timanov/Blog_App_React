e557df630d625c2b96f692e2cadf17bad80bd706
Mikhail Timanov <timanov.testing@gmail.com>
2022-10-06 13:48:16 +0300

    Добавление pages и роутинга библиотеки

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import './App.css'
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Route path="/" components={Home}/>
+      <Route path="/blog/:id" components={Blog}/>
     </div>
   )
 }
